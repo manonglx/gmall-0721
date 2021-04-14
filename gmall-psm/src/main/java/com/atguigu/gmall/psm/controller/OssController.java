a89f25154ad2f5657b4575cb333096ae4a2657a9
liuxi <liuxi@gitcloud.com.cn>
2021-04-14 22:50:19 +0800

    上传图片，获取阿里云oss密钥

---
+++ gmall-psm/src/main/java/com/atguigu/gmall/psm/controller/OssController.java
@@ -0,0 +1,83 @@
+package com.atguigu.gmall.psm.controller;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.utils.BinaryUtil;
+import com.aliyun.oss.model.MatchMode;
+import com.aliyun.oss.model.PolicyConditions;
+import com.atguigu.core.bean.Resp;
+import io.swagger.annotations.Api;
+import org.codehaus.jettison.json.JSONObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author 刘溪
+ * @date 2021/4/14 22:03
+ */
+@Api(tags = "阿里云处理图片")
+@RestController
+@RequestMapping("psm/oss")
+public class OssController {
+
+    /**
+     * 获得图片上传的签名
+     * @return
+     */
+    @GetMapping
+    public Resp<Object> policy(){
+
+        String accessId = "LTAI4GAACJqoRxVQtyyBaZ7o"; // 请填写您的AccessKeyId。
+        String accessKey = "<yourAccessKeySecret>"; // 请填写您的AccessKeySecret。
+        String endpoint = "oss-cn-shanghai.aliyuncs.com"; // 请填写您的 endpoint。
+        String bucket = "5gjishi"; // 请填写您的 bucketname 。
+        String host = "https://" + bucket + "." + endpoint; // host的格式为 bucketname.endpoint
+        // callbackUrl为 上传回调服务器的URL，请将下面的IP和Port配置为您自己的真实信息。
+//        String callbackUrl = "http://88.88.88.88:8888";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String dir = sdf.format(new Date()); // 用户上传文件时指定的前缀。
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessId, accessKey);
+        try {
+            long expireTime = 30;
+            long expireEndTime = System.currentTimeMillis() + expireTime * 1000;
+            Date expiration = new Date(expireEndTime);
+            // PostObject请求最大可支持的文件大小为5 GB，即CONTENT_LENGTH_RANGE为5*1024*1024*1024。
+            PolicyConditions policyConds = new PolicyConditions();
+            policyConds.addConditionItem(PolicyConditions.COND_CONTENT_LENGTH_RANGE, 0, 1048576000);
+            policyConds.addConditionItem(MatchMode.StartWith, PolicyConditions.COND_KEY, dir);
+
+            String postPolicy = ossClient.generatePostPolicy(expiration, policyConds);
+            byte[] binaryData = postPolicy.getBytes("utf-8");
+            String encodedPolicy = BinaryUtil.toBase64String(binaryData);
+            String postSignature = ossClient.calculatePostSignature(postPolicy);
+
+            Map<String, String> respMap = new LinkedHashMap<String, String>();
+            respMap.put("accessid", accessId);
+            respMap.put("policy", encodedPolicy);
+            respMap.put("signature", postSignature);
+            respMap.put("dir", dir);
+            respMap.put("host", host);
+            respMap.put("expire", String.valueOf(expireEndTime / 1000));
+            return  Resp.ok(respMap);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            ossClient.shutdown();
+        }
+        return  Resp.ok(null);
+    }
+
+
+}
